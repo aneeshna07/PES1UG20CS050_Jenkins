91eae2b40628ff9ed71c5a48d7203665a59c3d9c
ANEESH N A <71776979+aneeshna07@users.noreply.github.com>
2023-02-14 13:17:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  coutn << "Modified for Web hooks\n";
+  cout << "Modified for Web hooks\n";
   cout << "PES1UG20CS050 have successfully built and run\n";
   return 0;
 }
